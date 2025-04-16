efa1dd45f78f98bcc8a44539a9bce1fdaf257189
Sherif Ahmed <shreefahmed@gmail.com>
2025-04-17 01:12:12 +0200

    Added CPU Struct

--- Emu6502/Emu6502.cpp
+++ Emu6502/Emu6502.cpp
@@ -3,8 +3,38 @@
 
 #include <iostream>
 
+struct CPU
+{
+    using Byte = unsigned char;
+    using Word = unsigned short;
+
+    Word PC;        // program counter
+    Word SP;        // stack pointer
+
+    Byte A, X, Y;   // registers
+
+    Byte C : 1;     // status flag
+    Byte Z : 1;     // status flag
+    Byte I : 1;     // status flag
+    Byte D : 1;     // status flag
+    Byte B : 1;     // status flag
+    Byte V : 1;     // status flag
+    Byte N : 1;     // status flag
+
+    void Reset()
+    {
+        PC = 0xFFFC;
+        SP = 0x0100;
+        C = Z = I = D = B = V = N = 0;
+        A = X = Y = 0;
+    }
+};
+
+
 int main()
 {
+    CPU cpu;
+    cpu.reset();
     std::cout << "Hello World!\n";
 }
 
