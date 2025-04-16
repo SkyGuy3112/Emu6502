fcc0b6c1199754c7a61b7087b8b8284da9610b35
Sherif Ahmed <shreefahmed@gmail.com>
2025-04-17 01:30:00 +0200

    Added Memory and Basic FetchByte Operation

--- Emu6502/Emu6502.cpp
+++ Emu6502/Emu6502.cpp
@@ -3,11 +3,33 @@
 
 #include <iostream>
 
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using u32 = unsigned int;
+
+struct Mem
+{
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void Initialize()
+    {
+        for (u32 i = 0; i < MAX_MEM; i++)
+        {
+            Data[i] = 0;
+        }
+    }
+    
+    /* Read Byte from memory Operator */
+    Byte operator[](u32 Address) const
+    {
+        return Data[Address];
+    }
+};
+
 struct CPU
 {
-    using Byte = unsigned char;
-    using Word = unsigned short;
-
     Word PC;        // program counter
     Word SP;        // stack pointer
 
@@ -21,21 +43,38 @@ struct CPU
     Byte V : 1;     // status flag
     Byte N : 1;     // status flag
 
-    void Reset()
+    void Reset(Mem &memory)
     {
         PC = 0xFFFC;
         SP = 0x0100;
         C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
     }
+
+    Byte FetchByte(u32 Cycles, Mem& memory)
+    {
+        Byte Data = memory[PC];
+        PC++;
+        Cycles--;
+        return Data;
+    }
+
+    void Execute(u32 Cycles, Mem& memory)
+    {
+        while (Cycles > 0)
+        {
+            Byte Ins = FetchByte(Cycles, memory);
+        }
+    }
 };
 
 
 int main()
 {
+    Mem mem;
     CPU cpu;
-    cpu.reset();
-    std::cout << "Hello World!\n";
+    cpu.Reset(mem);
+    cpu.Execute(2, mem);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
